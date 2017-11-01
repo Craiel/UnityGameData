88a4f4767e24d9e40c394fced536b2409c90aa9e
Craiel <craieldev@gmail.com>
2017-11-01 01:16:23 -0400

    decoupling

---
+++ Contracts/IGameDataRuntimeResolver.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Scripts.Craiel.GameData.Contracts
+{
+    public interface IGameDataRuntimeResolver
+    {
+        GameDataId GetRuntimeId(GameDataRuntimeRefBase runtimeRef);
+    }
+}
