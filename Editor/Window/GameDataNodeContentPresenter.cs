f0e2ed2897e71b6a111ca16081337bbfe985476f
craiel <craieldev@gmail.com>
2018-02-21 17:45:18 -0500

    references

--- Editor/Window/GameDataNodeContentPresenter.cs
+++ Editor/Window/GameDataNodeContentPresenter.cs
@@ -1,5 +1,7 @@
 ﻿namespace Assets.Scripts.Craiel.GameData.Editor.Window
 {
+    // TODO: http://gram.gs/gramlog/creating-node-based-editor-unity/
+    // https://forum.unity.com/threads/simple-node-editor.189230/
     public class GameDataNodeContentPresenter : IGameDataContentPresenter
     {
         public void Draw(GameDataEditorContent content)
