5c4475a8ddfa743267c517ce04b668f7b7c7166d
Patrick Gustavel <craieldev@gmail.com>
2019-02-11 12:22:03 -0500

    Update GameDataObjectEditor.cs

--- Editor/Common/GameDataObjectEditor.cs
+++ Editor/Common/GameDataObjectEditor.cs
@@ -1,33 +1,18 @@
 namespace Craiel.UnityGameData.Editor.Common
 {
-    using System;
-    using System.Linq.Expressions;
     using UnityEditor;
     using UnityEngine;
-    using UnityEssentials.Editor.ReorderableList;
-    using UnityEssentials.Editor.UserInterface;
+    using UnityEssentials.Editor;
 
     [CustomEditor(typeof(GameDataObject))]
     [CanEditMultipleObjects]
-    public abstract class GameDataObjectEditor : Editor, IGameDataCompactEditor
+    public abstract class GameDataObjectEditor : EssentialCustomEditor, IGameDataCompactEditor
     {
         private static bool objectFoldout;
 
         // -------------------------------------------------------------------
         // Public
         // -------------------------------------------------------------------
-        public virtual bool UseDefaultInspector
-        {
-            get { return false; }
-        }
-
-        public override void OnInspectorGUI()
-        {
-            this.serializedObject.Update();
-            this.DrawFull();
-            this.serializedObject.ApplyModifiedProperties();
-        }
-        
         public virtual int GetCompactWidth()
         {
             return 200;
@@ -49,87 +34,23 @@ namespace Craiel.UnityGameData.Editor.Common
         // -------------------------------------------------------------------
         // Protected
         // -------------------------------------------------------------------
-        protected bool DrawFoldout(string title, ref bool toggle)
-        {
-            toggle = Layout.DrawSectionHeaderToggleWithSection(title, toggle);
-            return toggle;
-        }
-
-        protected virtual void DrawProperty<TSource>(Expression<Func<TSource, object>> expression, GUIContent content)
-        {
-            DrawProperty(this.serializedObject, expression, content);
-        }
-
-        protected virtual void DrawProperty<TSource>(Expression<Func<TSource, object>> expression, bool includeChildren = true)
-        {
-            DrawProperty(this.serializedObject, expression, includeChildren);
-        }
-
-        protected virtual void DrawProperty<TSource>(Expression<Func<TSource, object>> expression, params GUILayoutOption[] options)
-        {
-            DrawProperty(this.serializedObject, expression, options);
-        }
-
-        protected virtual void DrawProperty<TSource>(SerializedObject property, Expression<Func<TSource, object>> expression, GUIContent content = null)
-        {
-            DrawProperty(property.FindProperty(expression), null, true);
-        }
-
-        protected virtual void DrawProperty<TSource>(SerializedObject property, Expression<Func<TSource, object>> expression, bool includeChildren = true)
-        {
-            DrawProperty(property.FindProperty(expression), null, includeChildren);
-        }
-
-        protected virtual void DrawProperty<TSource>(SerializedObject property, Expression<Func<TSource, object>> expression, params GUILayoutOption[] options)
-        {
-            DrawProperty(property.FindProperty(expression), null, true, options);
-        }
-
-        protected virtual void DrawPropertyRelative<TSource>(SerializedProperty property, Expression<Func<TSource, object>> expression, bool includeChildren = true)
-        {
-            DrawProperty(property.FindPropertyRelative(expression), null, includeChildren);
-        }
-
-        protected virtual void DrawPropertyRelative<TSource>(SerializedProperty property, Expression<Func<TSource, object>> expression, params GUILayoutOption[] options)
-        {
-            DrawProperty(property.FindPropertyRelative(expression), null, true, options);
-        }
-
-        protected virtual void DrawProperty(SerializedProperty prop, GUIContent content, bool includeChildren, params GUILayoutOption[] options)
-        {
-            EditorGUILayout.PropertyField(prop, content, includeChildren, options);
-        }
-
-        protected virtual void DrawReorderableList<TSource>(string title, Expression<Func<TSource, object>> expression)
-        {
-            if (!this.serializedObject.isEditingMultipleObjects)
-            {
-                ReorderableListGUI.Title(title);
-                ReorderableListGUI.ListField(this.serializedObject.FindProperty(expression));
-            }
-        }
-
-        // -------------------------------------------------------------------
-        // Protected
-        // -------------------------------------------------------------------
-        protected virtual void DrawFull()
+        protected override void DrawFull()
         {
             if (this.UseDefaultInspector)
             {
-                base.OnInspectorGUI();
+                base.DrawFull();
+                return;
             }
-            else
+            
+            if (this.DrawFoldout("Object Properties", ref objectFoldout))
             {
-                if (this.DrawFoldout("Object Properties", ref objectFoldout))
-                {
-                    this.DrawProperty<GameDataObject>(x => x.Guid);
-                    this.DrawProperty<GameDataObject>(x => x.Name);
-                    this.DrawProperty<GameDataObject>(x => x.DisplayName);
-                    this.DrawProperty<GameDataObject>(x => x.Notes);
-                    this.DrawProperty<GameDataObject>(x => x.Description);
-                    this.DrawProperty<GameDataObject>(x => x.IconSmall);
-                    this.DrawProperty<GameDataObject>(x => x.IconLarge);
-                }
+                this.DrawProperty<GameDataObject>(x => x.Guid);
+                this.DrawProperty<GameDataObject>(x => x.Name);
+                this.DrawProperty<GameDataObject>(x => x.DisplayName);
+                this.DrawProperty<GameDataObject>(x => x.Notes);
+                this.DrawProperty<GameDataObject>(x => x.Description);
+                this.DrawProperty<GameDataObject>(x => x.IconSmall);
+                this.DrawProperty<GameDataObject>(x => x.IconLarge);
             }
         }
     }
