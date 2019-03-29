4da2327acf6a01ee9fb175b45e8605de02c728bb
Patrick Gustavel <craieldev@gmail.com>
2019-03-29 15:25:50 -0400

    Update IGameDataRuntimeValidationContext.cs

--- Runtime/Contracts/IGameDataRuntimeValidationContext.cs
+++ Runtime/Contracts/IGameDataRuntimeValidationContext.cs
@@ -1,11 +1,14 @@
 ﻿namespace Craiel.UnityGameData.Runtime.Contracts
 {
+    using JetBrains.Annotations;
+
     public delegate bool GameDataValidationFixDelegate(object owner, object entry);
     
     public interface IGameDataRuntimeValidationContext
     {
         void Warning(object owner, object source, GameDataValidationFixDelegate fixDelegate, string message);
 
+        [StringFormatMethod("message")]
         void WarningFormat(object owner, object source, GameDataValidationFixDelegate fixDelegate,
             string message, params object[] formatArguments);
 
